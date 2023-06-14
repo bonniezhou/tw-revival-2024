78760efb9aaea3e7f985c1477914314109fdc5c2
Nora Warschewski <93076262+norawarschewski@users.noreply.github.com>
2023-06-14 21:34:02 +0200

    .

--- assets/js/preloader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    var preloader = document.getElementById("preloader");
-    setTimeout(() => preloader.classList.add("hide"), 1000);
-  },
-  false
-);
